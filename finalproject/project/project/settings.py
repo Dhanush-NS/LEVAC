6a7c2f1b8ad335037658a99660582c0f7fac526c
Dhanush-NS <nsdhanush74@gmail.com>
2025-05-31 21:47:34 +0530

    code updated

--- finalproject/project/project/settings.py
+++ finalproject/project/project/settings.py
@@ -170,11 +170,3 @@ SESSION_CACHE_ALIAS = 'default'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Email Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nsdhanush5@gmail.com'  # Your Gmail address
-EMAIL_HOST_PASSWORD = 'koiw dnha rlkj ceqf'  # Use App Password if 2FA is enabled
-DEFAULT_FROM_EMAIL = 'nsdhanush5@gmail.com'  # Same as EMAIL_HOST_USER
