879e221efca7f23d105c585588668ee49044d48e
Dhanush-NS <nsdhanush74@gmail.com>
2025-07-22 17:24:02 +0530

    Prepare for Render deployment

--- finalproject/project/project/settings.py
+++ finalproject/project/project/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.getenv("django_secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []  # For initial testing, replace with your render URL later
+ALLOWED_HOSTS = ['*']  # For initial testing, replace with your render URL later
 
 
 
@@ -141,9 +141,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
 ]
 
 
